57eb28434e58136d4d90b3c894241c190a4d28dc
erikakuo <erikakuo@hotmail.com>
2022-03-30 11:43:23 -0300

    atualizando a rota

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './service/auth.service';
 
 @Component({
   selector: 'app-root', //caminho do seletor 
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'] //estilo CSS
 })
 export class AppComponent {
-  title = 'BlogPessoalThamires';
+  title = 'BlogPessoalErika';
+
+  constructor(
+    public auth: AuthService
+  ){}
 }
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { Usuario } from '../model/Usuario';
 
 @Injectable({
@@ -13,6 +14,19 @@ export class AuthService {
     Cadastrar(usuario: Usuario):Observable<Usuario>{
       return this.html.post<Usuario>('http://localhost:8080/usuarios/cadastrar', usuario);
 
+    /*}
+    Entrar(usuario: Entrar):Observable<Entrar>{
+      return this.html.post<Entrar>('http://localhost:8080/usuarios/entrar')
     }
-  }
 
+  }
+  logado(){
+    let ok: boolean = false
+
+    if(environment.token != ''){
+      ok = true
+    }
+    return ok
+  }*/
+}
+}
